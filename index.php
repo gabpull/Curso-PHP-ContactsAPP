b3c9b0d1513fd57d97a281c14f56ac7ba9d33921
Gabriel Alvarado <gab987.a@gmail.com>
2022-03-18 19:41:48 -0600

    Store contacts

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-  if(file_exists("contacts.json")){
-    $contacts = json_decode(file_get_contents("contacts.json"), true);
-  } else{
-    $contacts = [];
-  }
+
+  require "database.php";
+
+  $contacts = $conn->query("SELECT * FROM contacts");
+  //var_dump($contacts);
+  //die();
+
 ?>
 
 <!DOCTYPE html>
@@ -68,7 +70,7 @@
     <div class="container pt-4 p-3">
       <div class="row">
 
-      <?php if (count($contacts) == 0): ?>
+      <?php if ($contacts->rowCount() == 0): ?>
           <div class="col-md-4 mx-auto">
             <div class="card card-body text-center">
               <p>No contacts saved yet</p>
