7fbf514768203cd98f3b761fb02c18d82e196b8f
Gabriel Alvarado <gab987.a@gmail.com>
2022-03-18 22:00:54 -0600

    BORRAR contacto

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
                 <h3 class="card-title text-capitalize"><?= $contact["name"]      ?></h3>
                 <p class="m-2"><?= $contact["phone_number"]      ?></p>
                 <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-                <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
+                <a href="delete.php?id=<?= $contact["id"] ?>" class="btn btn-danger mb-2">Delete Contact</a>
               </div>
             </div>
           </div>
